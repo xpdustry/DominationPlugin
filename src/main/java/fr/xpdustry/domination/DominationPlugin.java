505b4e3d95b880c6310e0f7b4eb8911769c4e0c3
Phinner <62483793+Phinner@users.noreply.github.com>
2021-08-27 21:44:07 +0200

    hotfix for xasmedy

--- src/main/java/fr/xpdustry/domination/DominationPlugin.java
+++ src/main/java/fr/xpdustry/domination/DominationPlugin.java
@@ -2,41 +2,44 @@ package fr.xpdustry.domination;
 
 import arc.*;
 import arc.files.*;
+import arc.math.*;
+import arc.math.geom.*;
 import arc.struct.*;
+import arc.struct.IntFloatMap.*;
 import arc.util.*;
 import arc.util.serialization.*;
 import arc.util.serialization.JsonWriter.*;
 
 import mindustry.content.*;
+import mindustry.entities.*;
 import mindustry.game.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.Plugin;
 
-import fr.xpdustry.domination.DominationSettings.*;
-
-import java.util.*;
-import java.util.Map.*;
+import fr.xpdustry.domination.Zone.*;
 
 import static arc.util.Log.*;
 import static mindustry.Vars.*;
 
+
 @SuppressWarnings("unused")
 public class DominationPlugin extends Plugin{
     private static DominationSettings settings;
-
     private static final Json json = new Json();
     private static final Fi config = new Fi(Core.files.external("domination-config.json").absolutePath());
 
     private static final Interval interval = new Interval(3);
     private static final ObjectSet<Playerc> editors = new ObjectSet<>();
 
+    private static final IntFloatMap leaderboard = new IntFloatMap();
+    private static final Seq<Effect> effects = Seq.with(Fx.mine, Fx.mineBig, Fx.mineHuge);
+
     static {
         json.setOutputType(OutputType.json);
         json.setSerializer(DominationSettings.class, new DominationIO());
     }
 
-    /** The zones of the current map, might throw a NPE if used when not playing */
     private Seq<Zone> currentZones(){
         return settings.maps.get(state.map.name(), Seq::new);
     }
@@ -51,46 +54,90 @@ public class DominationPlugin extends Plugin{
             config.writeString(json.prettyPrint(settings));
         }
 
-        Events.run(WorldLoadEvent.class, () -> {
-            // updates the radius if it has been changed
-            currentZones().each(z -> z.setRadius(settings.zoneRadius));
-            interval.reset(2, 0); // Resets the timer when a new game begin
+        Events.run(PlayEvent.class, () -> {
+            state.rules.modeName = "[red]Domination";
         });
 
+        Events.run(WorldLoadEvent.class, () -> {
+            interval.reset(2, 0); // Reset the timer
+        });
+
+        // Main
         Events.run(Trigger.update, () -> {
-            if(state.rules.pvp && !state.gameOver){
-                if(interval.get(0, settings.updateTicks)){
-                    currentZones().forEach(z -> z.update(settings.captureRate));
-                    // Time remaining in seconds
-                    int time = (int) ((settings.gameDuration - interval.getTime(2)) / Time.toSeconds);
-                    Call.setHudText(Strings.format("Time remaining > @:@", time / 60, time % 60));
-                }if(interval.get(1, settings.renderTicks)){
-                    currentZones().forEach(z -> z.render(settings.updateTicks));
-                    // Shows the zone center to the editors
-                    editors.each(p -> {
-                        currentZones().each(z -> Call.effect(p.con(), Fx.heal, z.x * tilesize, z.y * tilesize, 0, z.getTeam().color));
-                    });
+            if(isActive() && interval.get(0, settings.updateTicks)){
+                // Updates the zone internal data
+                currentZones().each(z -> z.update(settings));
+
+                // Updates the leaderboard [team -> percent_captured]
+                leaderboard.clear(state.teams.active.size + 1);
+                currentZones().each(z -> leaderboard.increment(z.getTeam().id, 0, z.getPercent()));
+            }
+
+            if(isActive() && interval.get(1, settings.gameDuration)){
+                Entry winner = new Entry();
+                for(Entry entry : leaderboard.entries()){
+                    if(entry.value > winner.value){
+                        winner = entry;
+                    }
                 }
 
-                // Ugly way to determine the winner :^(
-                if(interval.get(2, settings.gameDuration)){
-                    Map<Team,Float> teams = new HashMap<>(state.teams.getActive().size); // using map for the compute method...
-                    currentZones().each(zone -> teams.compute(zone.getTeam(), (t, i) -> zone.getPercent() + (i != null ? i : 0)));
+                Events.fire(new GameOverEvent(Team.get(winner.key)));
+            }
 
-                    // Gets the highest captured percent
-                    float max = 0F;
-                    Team winner = Team.derelict;
-                    for(Entry<Team,Float> entry : teams.entrySet()){
-                        if(entry.getValue() > max){
-                            max = entry.getValue();
-                            winner = entry.getKey();
+            if(interval.get(2, Time.toSeconds / 6)){
+                // HUD text
+                StringBuilder builder = new StringBuilder(100);
+                // Generate a circle for the zone rendering
+                float[] circle = Geometry.regPoly((int)(Mathf.pi * settings.zoneRadius), settings.zoneRadius);
+
+                if(isActive()){
+                    currentZones().each(z -> {
+                        // Render the circle
+                        Geometry.iteratePolygon((cx, cy) -> {
+                            Call.effect(effects.random(), (cx + z.x) * tilesize, (cy + z.y) * tilesize, 0, z.getTeam().color);
+                        }, circle);
+
+                        // Display the percent in the circles
+                        String percent = Strings.format("[#@]@%", z.getTeam().color, Strings.fixed(z.getPercent(), 0));
+                        Call.label(percent, 1.0F / 6, z.x * tilesize, z.y * tilesize);
+                    });
+
+                    // Time remaining
+                    int time = (int) ((settings.gameDuration - interval.getTime(1)) / Time.toSeconds);
+                    builder.append(Strings.format("Time remaining > @\n", Strings.formatMillis(time * 1000L)));
+
+                    // Leaderboard
+                    var iterator = leaderboard.entries().iterator();
+                    while(iterator.hasNext()){
+                        var entry = iterator.next();
+                        var team = Team.get(entry.key);
+                        if(team != Team.derelict){
+                            builder.append(Strings.format("[#@]@[] > @%", team.color, team.name, (int)entry.value));
+                            if(iterator.hasNext()) builder.append('\n');
                         }
                     }
 
-                    // Wee
-                    Events.fire(new GameOverEvent(winner));
-                    Call.sendMessage(Strings.format("Congrats, @ team win!", winner));
+                    // Unclaimed zones
+                    if(leaderboard.containsKey(Team.derelict.id)){
+                        builder.append(Strings.format("\n[#@]Unclaimed[] > @%", Team.derelict.color, (int)leaderboard.get(Team.derelict.id)));
+                    }
+
+                    Call.setHudText(builder.toString());
                 }
+
+                // Rendering for editors
+                editors.each(p -> {
+                    currentZones().each(z -> {
+                        Call.effect(p.con(), Fx.unitLand, z.x * tilesize, z.y * tilesize, 0, z.getTeam().color);
+
+                        if(!state.rules.pvp && state.isGame()){
+                            // Render the circle
+                            Geometry.iteratePolygon((cx, cy) -> {
+                                Call.effect(p.con(), effects.random(), (cx + z.x) * tilesize, (cy + z.y) * tilesize, 0, z.getTeam().color);
+                            }, circle);
+                        }
+                    });
+                });
             }
         });
 
@@ -99,7 +146,6 @@ public class DominationPlugin extends Plugin{
             currentZones().each(Zone::reset);
             config.writeString(json.prettyPrint(settings));
         });
-
 
         Events.on(PlayerLeave.class, event -> {
             if(editors.contains(event.player)){
@@ -111,7 +157,7 @@ public class DominationPlugin extends Plugin{
             if(editors.contains(event.player)){
                 Zone zone = currentZones().find(z -> z.x == event.tile.x && z.y == event.tile.y);
                 if(zone == null){
-                    currentZones().add(new Zone(event.tile.x, event.tile.y, settings.zoneRadius));
+                    currentZones().add(new Zone(event.tile.x, event.tile.y));
                 }else{
                     currentZones().remove(zone);
                 }
@@ -121,18 +167,13 @@ public class DominationPlugin extends Plugin{
 
     @Override
     public void registerServerCommands(CommandHandler handler){
-        handler.register("domination-settings", "<save/load>", "Settings for the Domination plugin...", args -> {
+        handler.register("domination-config", "<save/load>", "Settings for the Domination plugin...", args -> {
             switch(args[0].toLowerCase()){
                 case "save" -> {
                     config.writeString(json.prettyPrint(settings));
                     info("Settings have been successfully saved.");
                 }
                 case "load" -> {
-                    if(state.isGame()){
-                        info("You can't just modify the settings in the middle of the game...");
-                        return;
-                    }
-
                     settings = json.fromJson(DominationSettings.class, config);
                     info("Settings have been successfully loaded.");
                 }
@@ -161,5 +202,17 @@ public class DominationPlugin extends Plugin{
                 default -> player.sendMessage(Strings.format("'@' is not a valid option.", args[0]));
             }
         });
+    }
+
+    public static boolean isActive(){
+        return state.rules.pvp && !state.isMenu();
+    }
+
+    static class DominationSettings{
+        public int zoneRadius = 5;
+        public float captureRate = 10F;
+        public float updateTicks = Time.toSeconds;
+        public float gameDuration = Time.toMinutes * 5;
+        public final ObjectMap<String, Seq<Zone>> maps = new ObjectMap<>();
     }
 }
--- src/main/java/fr/xpdustry/domination/Zone.java
+++ src/main/java/fr/xpdustry/domination/Zone.java
@@ -1,15 +1,12 @@
 package fr.xpdustry.domination;
 
-import arc.math.*;
-import arc.math.geom.*;
 import arc.struct.*;
 import arc.struct.IntIntMap.*;
-import arc.util.*;
 
-import mindustry.content.*;
-import mindustry.entities.*;
 import mindustry.game.*;
 import mindustry.gen.*;
+
+import fr.xpdustry.domination.DominationPlugin.*;
 
 import static mindustry.Vars.*;
 
@@ -18,26 +15,19 @@ public class Zone{
     public final int x;
     public final int y;
 
-    private transient int radius;
-    private transient float[] area;
-
     private transient Team team;
     private transient float percent;
     private transient final IntIntMap map;
 
-    private static final Seq<Effect> effects = Seq.with(Fx.mine, Fx.mineBig, Fx.mineHuge);
-
-    public Zone(int x, int y, int radius){
+    public Zone(int x, int y){
         this.x = x;
         this.y = y;
-        // Generates a circle for rendering
-        setRadius(radius);
-        this.map = new IntIntMap(state.teams.getActive().size);
+        this.map = new IntIntMap();
         // Set the default values for a new game
         reset();
     }
 
-    public void update(float captureRate){
+    public void update(DominationSettings settings){
         // Reset the team if the team got beaten
         if(!team.active()) team = Team.derelict;
         // Clears the map of the previous results
@@ -45,7 +35,7 @@ public class Zone{
 
         // Count the number of players in the zone, per team
         Groups.player.each(p -> {
-            if(p.within(x * tilesize, y * tilesize, radius * tilesize)){
+            if(p.within(x * tilesize, y * tilesize, settings.zoneRadius * tilesize)){
                 map.increment(p.team().id);
             }
         });
@@ -65,25 +55,12 @@ public class Zone{
         // Updates the zone values
         if(winner.key != 0 && !freeze){ // winner.key != 0 -> 0 is the id of derelict
             if(team.id == winner.key){
-                percent = Math.min(percent + captureRate, 100F);
+                percent = Math.min(percent + settings.captureRate, 100F);
             }else{
-                percent = Math.max(percent - captureRate, 0F);
+                percent = Math.max(percent - settings.captureRate, 0F);
                 if(percent == 0) team = Team.get(winner.key);
             }
         }
-    }
-
-    public void render(float updateTicks){
-        Geometry.iteratePolygon((cx, cy) -> {
-            Call.effect(effects.random(), (cx + x) * tilesize, (cy + y) * tilesize, 0, team.color);
-        }, area);
-
-        Call.label(Strings.format("[#@]@%", team.color, Strings.fixed(percent, 0)), updateTicks / Time.toSeconds, x * tilesize, y * tilesize);
-    }
-
-    public void setRadius(int radius){
-        this.radius = radius;
-        this.area = Geometry.regPoly((int)(this.radius * Mathf.pi), this.radius);
     }
 
     public Team getTeam(){
@@ -104,7 +81,6 @@ public class Zone{
     public String toString(){
         return "Zone{" + "x=" + x +
         ", y=" + y +
-        ", radius=" + radius +
         ", team=" + team +
         ", percent=" + percent +
         '}';
